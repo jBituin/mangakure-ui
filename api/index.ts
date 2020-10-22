2981546fec8e9d9f9374be33ff66ada7a6f22e27
Joemel <bituinjoemel@gmail.com>
2020-10-23 01:11:04 +0800

    Add endpoints for chapters and pages

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,15 @@
-import api from './api';
+import api from "./api";
 
 export default {
   getMangas: async () => {
     return await api.get(`/mangas`);
   },
+
+  getMangaChapters: async (mangaId: string) => {
+    return await api.get(`/manga-chapters/${mangaId}`);
+  },
+
+  getChapterPages: async (chapterId: string) => {
+    return await api.post(`/chapter-images`, { body: { chapterId } });
+  }
 };
