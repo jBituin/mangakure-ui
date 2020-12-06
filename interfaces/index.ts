c4f8d5e651921503f6c64f2b66086a520cc24d19
Joemel <bituinjoemel@gmail.com>
2020-12-06 15:14:42 +0800

    Update interfaces

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,13 +1,23 @@
 export interface Manga {
   readonly title: string;
   readonly coverImageUrl: string;
-  readonly url: string;
-  readonly created_at: Date;
   readonly synopsis: string;
   readonly author: string;
   readonly tags: string[];
   readonly slug: string;
-  readonly latestChapter: [];
+  readonly url?: string;
+  readonly created_at?: Date;
+  readonly latestChapter?: [];
+  readonly chapters?: Chapter[];
+}
+
+export interface Chapter {
+  readonly label: string;
+  readonly slug: string;
+  readonly url?: string;
+  readonly sequence?: number;
+  readonly mangaId?: string;
+  readonly mangaSlug?: string;
 }
 
 export interface Params {
